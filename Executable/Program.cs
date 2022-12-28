c9d8d70973c392212f2334d00b19510e37ab23c4
Antanas Jakubėnas <antanas.jakubenas@teliacompany.com>
2022-12-28 13:35:22 +0200

    Add project files.

---
+++ Executable/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations.Schema;
+
+Console.WriteLine("Hello, World!");
+
+
+SQLite.Database.Connect();
+
+
+Console.ReadLine();
---
+++ SQLite/Database.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace SQLite {
+	public static class Database {
+		public static void Connect() {
+			using (var connection = new SqliteConnection("Data Source=hello.db")) {
+				connection.Open();
+
+
+			}
+		}
+	}
+}
