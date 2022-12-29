74d759c37c64b27d1eac7a446e761d3238d771a2
RusysDev <info@rusys.lt>
2022-12-29 18:56:59 +0200

    some updates

--- Executable/Program.cs
+++ Executable/Program.cs
@@ -1,8 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.ComponentModel.DataAnnotations.Schema;
-
+﻿
 Console.WriteLine("Hello, World!");
-
 
 
 SQLite.Database.Connect();
--- SQLite/Database.cs
+++ SQLite/Database.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.Data;
 
 namespace SQLite {
 	public static class Database {
@@ -6,7 +7,25 @@ namespace SQLite {
 			using (var connection = new SqliteConnection("Data Source=hello.db")) {
 				connection.Open();
 
+				using var cmd = new SqliteCommand("select * from [test]", connection);
 
+				using var rdr = cmd.ExecuteReader();
+
+				while (rdr.Read()) {
+					var obj = new object[rdr.FieldCount];
+					rdr.GetValues(obj);
+
+					Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(obj));
+				}
+
+				//using var cmd = new SQLiteCommand(con);
+				//cmd.CommandText = "INSERT INTO cars(name, price) VALUES(@name, @price)";
+
+				//cmd.Parameters.AddWithValue("@name", "BMW");
+				//cmd.Parameters.AddWithValue("@price", 36600);
+				//cmd.Prepare();
+
+				//cmd.ExecuteNonQuery();
 			}
 		}
 	}
